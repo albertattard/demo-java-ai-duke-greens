858da6af2f901cffe8d80ee58e26974889ecf39a
Albert Attard <albertattard@gmail.com>
2026-07-15 07:45:28 +0200

    Add deployment helper script

---
+++ tools/deplo.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+repository_root="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+cd "$repository_root"
+./mvnw clean verify
+
+cd infrastructure/terraform
+terraform apply -replace=terraform_data.application
+
+curl --fail --location https://javaworkshopshub.dev/
+curl --fail --location https://javaworkshopshub.dev/demo-access

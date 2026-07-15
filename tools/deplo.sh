b71f334c74a2b8be8790445b8e4221c925698fc4
Albert Attard <albertattard@gmail.com>
2026-07-15 10:36:52 +0200

    Automate Terraform approval in deployment

--- tools/deplo.sh
+++ tools/deplo.sh
@@ -8,7 +8,7 @@ cd "$repository_root"
 ./mvnw clean verify
 
 cd infrastructure/terraform
-terraform apply -replace=terraform_data.application
+terraform apply -auto-approve -replace=terraform_data.application
 
 curl --fail --location https://javaworkshopshub.dev/
 curl --fail --location https://javaworkshopshub.dev/demo-access
